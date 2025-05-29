7d55fdcf0c72098ac0330d30661655bbf38e2d62
moustaphisene <32897441+moustaphisene@users.noreply.github.com>
2025-05-29 20:28:18 +0000

    adapte to local authserveru

--- src/main/java/sen/bank/demospring6security/config/KeycloakRollConverter.java
+++ src/main/java/sen/bank/demospring6security/config/KeycloakRollConverter.java
@@ -7,8 +7,6 @@ import org.springframework.security.oauth2.jwt.Jwt;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 public class KeycloakRollConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
@@ -19,13 +17,23 @@ public class KeycloakRollConverter implements Converter<Jwt, Collection<GrantedA
      */
     @Override
     public Collection<GrantedAuthority> convert(Jwt source) {
-        Map<String, Object> realmAccess = (Map<String, Object>) source.getClaims().get("realm_access");
-        if (realmAccess == null || realmAccess.isEmpty()) {
+//        Map<String, Object> realmAccess = (Map<String, Object>) source.getClaims().get("realm_access");
+//        if (realmAccess == null || realmAccess.isEmpty()) {
+//            return new ArrayList<>();
+//        }
+//
+//        Collection<GrantedAuthority> returnValue = ((List<String>)realmAccess.get("roles"))
+//                .stream().map(roleName -> "ROLE_" + roleName)
+//                .map(SimpleGrantedAuthority::new)
+//                .collect(Collectors.toList());
+//        return returnValue;
+//    }
+        ArrayList<String> roles = (ArrayList<String>) source.getClaims().get("scope");
+        if (roles == null || roles.isEmpty()) {
             return new ArrayList<>();
         }
 
-        Collection<GrantedAuthority> returnValue = ((List<String>)realmAccess.get("roles"))
-                .stream().map(roleName -> "ROLE_" + roleName)
+        Collection<GrantedAuthority> returnValue = roles.stream().map(roleName -> "ROLE_" + roleName)
                 .map(SimpleGrantedAuthority::new)
                 .collect(Collectors.toList());
         return returnValue;
